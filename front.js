b809a5519414a7c3a7194f13d7c84679f03e0f6f
Luisa-VT <luisa.m.truembach@stud.h-da.de>
2021-07-13 18:16:06 +0200

    videos eingebunden

--- front.js
+++ front.js
@@ -1,3 +1,25 @@
 function mit() {
-    document.getElementById("img").src = "img/Fernseher.jpeg"
+    document.getElementById("vidMit").style.display = "block";
+    document.getElementById("mitButton").style.display ="none";
+    document.getElementById("ohneButton").style.display ="none";
+    document.getElementById("img").replaceWith(document.getElementById("vidMit"));
+
+    document.getElementById("vidMit").onended = function nextMit(){
+        document.getElementById("vidMit").style.display = "none";
+        document.getElementById("Fernseher1").style.display = "block";
+        document.getElementById("vidMit").replaceWith(document.getElementById("Fernseher1"));
+      }
   }
+  function ohne(){
+    document.getElementById("vidOhne").style.display = "block";
+    document.getElementById("mitButton").style.display ="none";
+    document.getElementById("ohneButton").style.display ="none";
+    document.getElementById("img").replaceWith(document.getElementById("vidOhne"));
+
+    document.getElementById("vidOhne").onended = function nextMit(){
+        document.getElementById("vidOhne").style.display = "none";
+        document.getElementById("Fernseher1").style.display = "block";
+        document.getElementById("vidOhne").replaceWith(document.getElementById("Fernseher1"));
+      }
+  }
+ 
