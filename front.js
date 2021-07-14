db49c90a80e8224fde5b6d1c88a779c1a70aafa3
Luisa-VT <luisa.m.truembach@stud.h-da.de>
2021-07-14 12:51:46 +0200

    Token auswahl hinzugefügt

--- front.js
+++ front.js
@@ -1,3 +1,4 @@
+let auswahlliste = [];
 function mit() {
     document.getElementById("vidMit").style.display = "block";
     document.getElementById("mitButton").style.display ="none";
@@ -8,8 +9,11 @@ function mit() {
 
     document.getElementById("vidMit").onended = function nextMit(){
         document.getElementById("vidMit").style.display = "none";
-        document.getElementById("Fernseher1").style.display = "block";
-        document.getElementById("vidMit").replaceWith(document.getElementById("Fernseher1"));
+        document.getElementById("imgTokens").style.display = "block";
+        document.getElementById("auswahlT1").style.display = "block";
+        document.getElementById("auswahlT2").style.display = "block";
+        document.getElementById("auswahlT3").style.display = "block";
+        document.getElementById("vidMit").replaceWith(document.getElementById("imgTokens"));
       }
   }
   function ohne(){
@@ -23,7 +27,21 @@ function mit() {
     document.getElementById("vidOhne").onended = function nextMit(){
         document.getElementById("vidOhne").style.display = "none";
         document.getElementById("Fernseher1").style.display = "block";
+        document.getElementById("tv").style.display = "block";
         document.getElementById("vidOhne").replaceWith(document.getElementById("Fernseher1"));
       }
+  }
+
+  function auswahl1(){
+    document.getElementById("auswahlT1").style.color ="blue";
+    auswahlliste.push("Regen");
+  }
+  function auswahl2(){
+    document.getElementById("auswahlT2").style.color ="green";
+    auswahlliste.push("Haare");
+  }
+  function auswahl3(){
+    document.getElementById("auswahlT3").style.color ="white";
+    auswahlliste.push("Pinguin");
   }
  
